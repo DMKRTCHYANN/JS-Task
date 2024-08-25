420b08a3f7af29e727514d4cd3bfb87ca9ec8664
DMKRTCHYANN <mkrtchyandavv@gmail.com>
2024-08-25 19:20:46 +0400

    toTitleCase

---
+++ toTitleCase.js
@@ -0,0 +1,9 @@
+function toTitleCase(str) {
+    return str
+        .split(' ')
+        .map(word =>
+            word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(' ');
+}
+console.log(toTitleCase(" j s"));
